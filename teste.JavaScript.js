c81df6aabf1337c87b2080efa66f4d24b417dc28
Alesson Barauna <alesson.barauna1@gmail.com>
2022-07-08 11:23:37 -0300

    exemplos da aula 1

---
+++ teste.JavaScript.js
@@ -0,0 +1,80 @@
+
+/* // Tipo Number
+const Meunumero = 3;
+
+const Primerionumero = 5;
+const SegundoNumero = 10;
+
+const PrimeiraOperacao = Primerionumero / SegundoNumero;
+
+// console.log(PrimeiraOperacao)
+
+
+// Tipo Flutuante
+
+const NumeroTipoFlutuante = 5.5;
+
+const NumeroTipoFlutuanteSemZero = .5;
+
+const NovaOperacao = NumeroTipoFlutuante / NumeroTipoFlutuanteSemZero;
+
+// console.log(NovaOperacao)
+
+
+// Tipe String
+
+const texto1 = "Olá, Mundo!";
+const texto2 =  'Olá, Mundo2!';
+const senha = "senhaSuperSegura123!";
+const StringDeNumeros = "123456";
+
+const citacao = 'O Alesson disse "oi"';
+const citacao2 = "A Karol Respondeu 'olá'";
+console.log(citacao)
+console.log(citacao2)
+
+const citacao3 = "Meu nome é ";
+const MeuNome = "Alesson!";
+console.log(citacao3 + MeuNome)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte"; 
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres */
+
+// Boolean
+// True -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivo = true;
+
+//console.log(primeiroNumero === segundoNumero)
+
+const texto1 = "Alura";
+const texto2 = "Alure";
+const texto3 = "Alura";
+
+console.log(texto1 === texto2)
+console.log(texto1 === texto3)
