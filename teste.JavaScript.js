123d5c13ff238a4377d9b58f161a92bd2caf765d
Alesson Barauna <alesson.barauna1@gmail.com>
2022-07-12 11:24:55 -0300

    CursoAlura

--- teste.JavaScript.js
+++ teste.JavaScript.js
@@ -66,7 +66,7 @@ console.log(senha.length) // 13 caracteres */
 // True -> verdadeiro
 // false -> falso
 
-const primeiroNumero = 5;
+/*const primeiroNumero = 5;
 const segundoNumero = 5;
 let cadastroAtivo = true;
 
@@ -77,4 +77,75 @@ const texto2 = "Alure";
 const texto3 = "Alura";
 
 console.log(texto1 === texto2)
-console.log(texto1 === texto3)
+console.log(texto1 === texto3)*/
+
+//var 
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area)
+//var area;
+
+/* let forma = 'retangulo'
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if (forma === 'retangulo') {
+    area = altura * comprimento;
+}else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)*/
+
+/* const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+}else {
+    area = altura * comprimento;
+}
+
+
+console.log(area)*/
+
+
+// boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+// "truthy ou falsy"
+
+// valores que o JavaScript pode considerar como false
+ 
+// 0 => false
+// 1 => true
+
+/*console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)*/
+
+// undfined
+
+// null ==> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = "Alura";
+//typeof é uma palavra chave para perguntarmos para o javascript qual tipo de dado que esta sendo guardado nessa variavel
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
+
+// null é um objeto
